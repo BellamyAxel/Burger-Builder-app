c5416ff9a5171511d5bdc592b1938ec644304186
Shokhrukhmirzo <a.bellamy@inbox.ru>
2019-03-16 21:18:57 +0500

    Updates

---
+++ precache-manifest.c726f7d18d1aa9cbb64faa3323a75ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2c6325e620a3a582bd",
+    "url": "/Burger-Builder-app/static/css/main.c4e4560d.chunk.css"
+  },
+  {
+    "revision": "2c2c6325e620a3a582bd",
+    "url": "/Burger-Builder-app/static/js/main.2c2c6325.chunk.js"
+  },
+  {
+    "revision": "c4f5dd2267684cda0ab0",
+    "url": "/Burger-Builder-app/static/js/1.c4f5dd22.chunk.js"
+  },
+  {
+    "revision": "423152c53b6f069446e3",
+    "url": "/Burger-Builder-app/static/js/runtime~main.423152c5.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "f47d517660bdb92218a2b807f89cecf3",
+    "url": "/Burger-Builder-app/index.html"
+  }
+];
